009d40fc9a7dd7baba225584e7151ceb871dee7d
Parve Artem <artparve@gmail.com>
2022-02-11 11:33:13 +0300

    Change 02

--- lesson_01/02.c
+++ lesson_01/02.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 void main2()
 {
-    printf("Hello world!\n");
+    int n;
+    int a;
+
+    n = scanf("%d", &a);
+    if (n != 1){
+        return 1;
+    }
+
+    if ((a % 2) == 0){
+        printf("Это число четное(Yes)");
+    }
+    else{
+        printf("Это число нечетное(No)");
+    }
 }
--- lesson_01/main.c
+++ lesson_01/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    main1();
-    //main2();
+    //main1();
+    main2();
     return 0;
 }
