3d06b821558fabb580239123d7733bb19bbbe2e9
Parve Artem <artparve@gmail.com>
2022-02-22 10:18:32 +0300

    Add 02_2

---
+++ lesson_02/02_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main02_2(){
+    int a = 12;
+
+    while(a < 100){
+        printf("%d\n", a);
+        a += 3;
+    }
+}
--- lesson_02/main.c
+++ lesson_02/main.c
@@ -5,6 +5,7 @@ int main()
 {
     //main01_1();
     //main01_2();
-    main02_1();
+    //main02_1();
+    main02_2();
     return 0;
 }
